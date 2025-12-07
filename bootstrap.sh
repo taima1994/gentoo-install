7a021c9e70fadb013cf062da416ffa135846dfd5
taima <tantai01091994@gmail.com>
2025-12-07 12:27:12 +0700

    Download additional installation scripts
    Added multiple wget commands to download additional scripts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,21 @@ echo "Tải và chạy installer..."
 # 1. Tải script
 wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/ghost-installer-full.sh
 wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/fix-3.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/1.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/2.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/3.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/4.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/5.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/6.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/7.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/8.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/9.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/10.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/11.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/12.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/13.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/14.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/15.sh
 
 # 2. Cấp quyền thực thi
 chmod +x ghost-installer-full.sh
