ff838012d5444d598bb740e6b7965ee366490d7c
taima <tantai01091994@gmail.com>
2025-12-07 12:31:02 +0700

    bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,42 +3,42 @@ set -e
 echo "GHOST 2025 - GENTOO INSTALL"
 echo "Tải và chạy installer..."
 # 1. Tải script
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/ghost-installer-full.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/fix-3.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/0.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/1.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/2.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/3.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/4.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/5.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/6.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/7.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/8.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/9.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/10.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/11.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/12.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/13.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/14.sh
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/15.sh
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/ghost-installer-full.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/fix-3.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/0.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/1.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/2.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/3.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/4.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/5.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/6.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/7.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/8.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/9.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/10.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/11.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/12.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/13.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/14.sh &&
+wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/15.sh &&
 
 # 2. Cấp quyền thực thi
-chmod +x ghost-installer-full.sh
-chmod +x fix-3.sh
-chmod +x 0.sh
-chmod +x 1.sh
-chmod +x 2.sh
-chmod +x 3.sh
-chmod +x 4.sh
-chmod +x 5.sh
-chmod +x 6.sh
-chmod +x 7.sh
-chmod +x 8.sh
-chmod +x 9.sh
-chmod +x 10.sh
-chmod +x 11.sh
-chmod +x 12.sh
-chmod +x 13.sh
-chmod +x 14.sh
-chmod +x 15.sh
+chmod +x ghost-installer-full.sh &&
+chmod +x fix-3.sh &&
+chmod +x 0.sh &&
+chmod +x 1.sh &&
+chmod +x 2.sh &&
+chmod +x 3.sh &&
+chmod +x 4.sh &&
+chmod +x 5.sh &&
+chmod +x 6.sh &&
+chmod +x 7.sh &&
+chmod +x 8.sh &&
+chmod +x 9.sh &&
+chmod +x 10.sh &&
+chmod +x 11.sh &&
+chmod +x 12.sh &&
+chmod +x 13.sh &&
+chmod +x 14.sh &&
+chmod +x 15.sh &&
 
