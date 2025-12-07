0be5396dfbc64eca5425283bba14d0d6bd877ab1
taima <tantai01091994@gmail.com>
2025-12-07 04:37:50 +0000

    Update fix-openmp-kernel-para.sh

--- fix-openmp-kernel-para.sh
+++ fix-openmp-kernel-para.sh
@@ -1,5 +1,12 @@
-# Dòng 129-134
-cat >> /etc/portage/package.use/hyprland << 'EOF'
-gui-wm/hyprland -systemd
-x11-terms/kitty -wayland
-EOF
+# Mask systemd
+echo "sys-apps/systemd" >> /etc/portage/package.mask
+echo "virtual/systemd" >> /etc/portage/package.mask
+
+# Cấu hình use flags
+echo "sys-apps/dbus -systemd elogind" >> /etc/portage/package.use
+echo "sys-apps/util-linux -systemd" >> /etc/portage/package.use
+echo "virtual/libudev -systemd" >> /etc/portage/package.use
+
+# Update với --autounmask
+emerge --update --deep --newuse @world --autounmask-write
+dispatch-conf
