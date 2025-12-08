dd620051e712e4680f3c25cea79e0ea12dfe11ba
taima <tantai01091994@gmail.com>
2025-12-08 14:27:45 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,11 @@
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/ghost-installer-full.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/ghost-installer-full-fixed.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/full-install.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/fix-3.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/0.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/1.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/2.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/3.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/4.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/5.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/6.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/7.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/8.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/9.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/10.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/11.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/12.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/13.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/14.sh &&
-wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/15.sh
+#!/bin/bash
+
+set -e
+
+# Repo bạn muốn tải
+REPO_URL="https://github.com/taima1994/gentoo-install.git"
+
+echo "[+] Cloning repository..."
+git clone "$REPO_URL"
+
+echo "[+] Done!"
