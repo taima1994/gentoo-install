3bdba0a93a0f97c2ba9b083df9aef0d8518c963d
taima <tantai01091994@gmail.com>
2025-12-08 14:23:51 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-set -e
-echo "GHOST 2025 - GENTOO INSTALL"
-echo "Tải và chạy installer..."
-# 1. Tải script
 wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/ghost-installer-full.sh &&
 wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/ghost-installer-full-fixed.sh &&
 wget https://raw.githubusercontent.com/taima1994/gentoo-install/main/full-install.sh &&
