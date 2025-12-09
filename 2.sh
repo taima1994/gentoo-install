612a55211989106ccf870ae4da1a7921894fae85
taima <tantai01091994@gmail.com>
2025-12-09 12:35:45 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,9 +1,14 @@
-# 2. Format và mount
-echo "2. FORMAT + MOUNT..."
-mkfs.ext4 -F /dev/sda1
-mkfs.ext4 -F /dev/sda2
-mkfs.ext4 -F /dev/sdb1
-mount /dev/sda1 /mnt/gentoo
-mkdir -p /mnt/gentoo/{home,var/tmp/portage}
-mount /dev/sda2 /mnt/gentoo/home
-mount /dev/sdb1 /mnt/gentoo/var/tmp/portage
+# 3.1 Chuyển vào thư mục mount
+cd /mnt/gentoo
+
+# 3.2 Tải stage3 mới nhất (chọn đúng kiến trúc)
+links https://www.gentoo.org/downloads/
+# Hoặc dùng wget:
+wget https://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-desktop-openrc.txt
+# Xem file vừa tải để biết tên stage3 chính xác
+
+# 3.3 Tải stage3
+wget [URL_STAGE3_TỪ_FILE_TRÊN]
+
+# 3.4 Giải nén
+tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
