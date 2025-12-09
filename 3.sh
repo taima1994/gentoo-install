2e69f73d351d2968fd3a19c97946b5c7015d3b40
taima <tantai01091994@gmail.com>
2025-12-09 11:54:28 +0700

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -3,7 +3,7 @@ echo "3. TẢI STAGE3 + VERIFY..."
 cd /mnt/gentoo
 STAGE3="stage3-amd64-hardened-selinux-openrc-20251130T164554Z.tar.xz"
 wget -c https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-hardened-selinux-openrc/$STAGE3 || wget -c https://mirror.meowsmp.net/gentoo/releases/amd64/autobuilds/current-stage3-amd64-hardened-selinux-openrc/$STAGE3
-wget -c https://distfiles.gentoo.org/snapshots/current/portage-latest.tar.xz
+wget -c https://mirror.meowsmp.net/gentoo/snapshots/portage-latest.tar.xz
 tar xpf $STAGE3 --xattrs-include="*.*" --numeric-owner
 tar xpf portage-latest.tar.xz -C usr
 echo "STAGE3 + PORTAGE TẢI XONG!"
