82ccb255fe3a0515f1adbb631cda96c13c6a2991
taima <tantai01091994@gmail.com>
2025-12-09 12:39:17 +0700

    Update 8.sh

--- 8.sh
+++ 8.sh
@@ -1,3 +1,8 @@
-# 8. Cài Catalyst + tool build Ghost
-echo "8. Cài Catalyst + layman + icecc + ccache"
-emerge sys-apps/catalyst app-portage/layman sys-process/icecc sys-process/ccache
+# 8.1 Cài GRUB
+emerge --ask sys-boot/grub
+
+# 8.2 Cài đặt GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot
+
+# 8.3 Tạo config
+grub-mkconfig -o /boot/grub/grub.cfg
