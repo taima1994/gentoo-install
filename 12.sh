0df53050516461edd23223dcac2fb577e0d2bcc7
taima <tantai01091994@gmail.com>
2025-12-09 12:41:22 +0700

    Delete 12.sh

--- 12.sh
+++
@@ -1,8 +0,0 @@
-# 12. Timezone + locale
-echo "Asia/Ho_Chi_Minh" > /etc/timezone
-emerge --config sys-libs/timezone-data
-echo "vi_VN.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-eselect locale set vi_VN.utf8
-env-update && source /etc/profile
