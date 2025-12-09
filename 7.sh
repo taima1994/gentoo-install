824fa233023c9a3e2cbe664758926b7bed48fa79
taima <tantai01091994@gmail.com>
2025-12-09 12:38:53 +0700

    Update 7.sh

--- 7.sh
+++ 7.sh
@@ -1,7 +1,24 @@
-# 7. Cài Hyprland + theme Ghost
-echo "7. Cài Hyprland..."
-cat >> /etc/portage/package.use/hyprland << 'EOF'
-gui-wm/hyprland -systemd
-x11-terms/kitty -wayland
-EOF
-emerge gui-wm/hyprland x11-terms/kitty waybar wofi mako rofi-lbonn-wayland
+# 7.1 Tạo file fstab
+blkid  # Lấy UUID các partition
+nano /etc/fstab
+# Thêm các dòng:
+# UUID=[UUID_ROOT] / ext4 defaults,noatime 0 1
+# UUID=[UUID_BOOT] /boot vfat defaults 0 2
+# UUID=[UUID_SWAP] none swap sw 0 0
+
+# 7.2 Đặt hostname
+nano /etc/conf.d/hostname
+# hostname="gentoo"
+
+# 7.3 Cấu hình network
+emerge --ask net-misc/dhcpcd
+# Hoặc netifrc:
+emerge --ask net-misc/netifrc
+
+# Cấu hình cho eth0:
+cd /etc/init.d
+ln -s net.lo net.eth0
+rc-update add net.eth0 default
+
+# 7.4 Đặt root password
+passwd
