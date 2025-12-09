3060122ad5abb264e59b9afcc308b5ffc029a3bf
taima <tantai01091994@gmail.com>
2025-12-09 12:38:25 +0700

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,18 @@
+# 6.1 Cài đặt kernel sources
+emerge --ask sys-kernel/gentoo-sources
 
-echo "6. Cài firmware (sau kernel)"
-emerge =sys-kernel/linux-firmware-20250808
+# 6.2 Cài đặt genkernel (cho dễ)
+emerge --ask sys-kernel/genkernel
+
+# 6.3 Sao chép config mặc định
+cd /usr/src/linux
+zcat /proc/config.gz > .config
+
+# 6.4 Cấu hình kernel (tùy chọn)
+# make menuconfig  # Nếu muốn tùy chỉnh
+
+# 6.5 Biên dịch kernel với genkernel
+genkernel all
+
+# 6.6 Cài đặt firmware
+emerge --ask sys-kernel/linux-firmware
