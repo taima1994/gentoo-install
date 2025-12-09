564c0748169f54463f9fd7de605cc3ddf02febf1
taima <tantai01091994@gmail.com>
2025-12-09 12:40:05 +0700

    Update 9.sh

--- 9.sh
+++ 9.sh
@@ -1,6 +1,16 @@
-# 9. Tạo user ghost + sudo
-echo "9. Tạo user ghost..."
-useradd -m -G wheel,audio,video,portage ghost
-echo "ghost:ghost" | chpasswd
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel
-chmod 440 /etc/sudoers.d/wheel
+# 9.1 Cài đặt tools hệ thống
+emerge --ask app-admin/sysklogd
+rc-update add sysklogd default
+
+emerge --ask sys-process/cronie
+rc-update add cronie default
+
+# 9.2 Cài đặt SSH
+emerge --ask net-misc/openssh
+rc-update add sshd default
+
+# 9.3 Cài đặt file indexing
+emerge --ask sys-apps/mlocate
+
+# 9.4 Cài đặt sudo
+emerge --ask app-admin/sudo
