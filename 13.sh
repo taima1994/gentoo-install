6c9c893a07f8127db4e1688cc81f087393c4acbb
taima <tantai01091994@gmail.com>
2025-12-10 11:05:07 +0000

    Update 13.sh

--- 13.sh
+++ 13.sh
@@ -1,8 +1,22 @@
-# Login với user 'long'
-sudo emerge --sync
-sudo emerge -avuDN @world
-sudo emerge --depclean
-sudo eselect news read all
+# 1. XEM TÌNH TRẠNG HIỆN TẠI
+mount | grep /mnt/gentoo
 
-# Tools for ISO building
-sudo emerge app-cdr/cdrtools squashfs-tools sys-fs/squashfuse
+# 2. UNMOUNT TẤT CẢ
+umount -R /mnt/gentoo
+
+# 3. MOUNT LẠI TỪ ĐẦU - ĐÚNG CHUẨN
+mount /dev/sdb3 /mnt/gentoo                      # root
+mount /dev/sdb1 /mnt/gentoo/boot                 # boot (SSD 1G)
+mount /dev/sdb4 /mnt/gentoo/var/tmp/portage      # portage temp
+mount /dev/sda1 /mnt/gentoo/home                 # home
+mount /dev/sda2 /mnt/gentoo/var/cache/binpkgs    # binpkgs
+mount /dev/sda3 /mnt/gentoo/var/db/repos         # portage tree
+mount /dev/sda4 /mnt/gentoo/mnt/iso-storage      # iso storage
+
+# 4. KIỂM TRA
+mount | grep /mnt/gentoo
+# Phải thấy:
+# /dev/sdb3 on /mnt/gentoo type ext4
+# /dev/sdb1 on /mnt/gentoo/boot type vfat
+# /dev/sda1 on /mnt/gentoo/home type ext4
+# KHÔNG có 2 boot!
