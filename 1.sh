cb3c86eb211e68ef0170814230879a97f60e70dc
taima <tantai01091994@gmail.com>
2025-12-10 15:11:36 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -95,3 +95,19 @@ wget [URL_STAGE3_TỪ_FILE_TRÊN]
 
 # 3.4 Giải nén
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
+
+# 4.1 Sao chép DNS info
+cp -L /etc/resolv.conf /mnt/gentoo/etc/
+
+# 4.2 Mount các filesystem cần thiết
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+
+# 4.3 Chroot vào hệ thống mới
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+
