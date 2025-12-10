2e4e9d9afd898616929edfbfb6080d8544556fde
taima <tantai01091994@gmail.com>
2025-12-10 11:07:02 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,16 @@
+# 1.1 Boot từ USB/LiveCD
+# Chọn option boot phù hợp với UEFI/Legacy
+
+# 1.2 Kiểm tra kết nối mạng
+ping -c 4 google.com
+
+# 1.3 Kiểm tra lại
+ip addr show
+ping -c 3 gentoo.org
+
+# 1.5 Cập nhật date/time
+date
+
 #!/bin/bash
 # AUTO PARTITION FOR GENTOO BUILD SYSTEM
 # SSD: sdb (223G) - HDD: sda (931G)
