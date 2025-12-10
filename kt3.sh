50d44bd195a0ac173bfc52132a99f8b08287c8f9
taima <tantai01091994@gmail.com>
2025-12-10 15:30:04 +0700

    Delete kt3.sh

--- kt3.sh
+++
@@ -1,9 +0,0 @@
-
-# Chroot vào
-mount /dev/sdb3 /mnt/gentoo
-mount /dev/sdb1 /mnt/gentoo/boot
-mount /dev/sda1 /mnt/gentoo/home
-# ... mount các phần còn lại
-
-chroot /mnt/gentoo /bin/bash
-source /etc/profile
