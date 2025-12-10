fcb2974484bcb5d5e93344823926933e2c6cbdff
taima <tantai01091994@gmail.com>
2025-12-10 15:16:28 +0700

    Update kt1.sh

--- kt1.sh
+++ kt1.sh
@@ -1,12 +1,3 @@
-# Xem mount hiện tại
-mount | grep /boot
-
-# Unmount cái sai
-umount /boot
-
-# Mount đúng phân vùng boot
-mount /dev/sdb1 /boot
-
-# Kiểm tra lại
-lsblk
-df -h | grep boot
+# Unmount các phân vùng sai khỏi /boot
+umount /dev/sdb2  # Đây là SWAP, không phải boot!
+umount /dev/sda1  # Đây là /home, không phải boot!
