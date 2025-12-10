a2bdc0b82c72e5f7a1f6a8ebdba0aeb437541db0
taima <tantai01091994@gmail.com>
2025-12-10 13:55:40 +0700

    Update 8.sh

--- 8.sh
+++ 8.sh
@@ -6,3 +6,16 @@ grub-install --target=x86_64-efi --efi-directory=/boot
 
 # 8.3 Tạo config
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Add user
+useradd -m -G wheel,audio,video -s /bin/bash long
+passwd long
+
+# Sudo
+emerge --ask app-admin/sudo
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# Services
+emerge --ask sys-process/cronie net-misc/openssh
+rc-update add cronie default
+rc-update add sshd default
