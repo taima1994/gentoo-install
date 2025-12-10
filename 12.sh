6cb5ecb532d803a1e027ecf38ccfee55ef7c1ac9
taima <tantai01091994@gmail.com>
2025-12-10 11:16:41 +0000

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -1,6 +1,17 @@
 # Thoát chroot
 exit
-cd /
-umount -l /mnt/gentoo/dev{/shm,/pts,}
+
+# Unmount theo thứ tự
+umount /mnt/gentoo/var/tmp/portage
+umount /mnt/gentoo/var/cache/binpkgs
+umount /mnt/gentoo/var/db/repos
+umount /mnt/gentoo/home
+umount /mnt/gentoo/mnt/iso-storage
+umount /mnt/gentoo/boot
+umount /mnt/gentoo
+
+# Hoặc dùng lệnh nhanh
 umount -R /mnt/gentoo
+
+# Reboot
 reboot
