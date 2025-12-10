82613030bb72e821e2118f5977729afc075b0441
taima <tantai01091994@gmail.com>
2025-12-10 09:02:08 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -80,21 +80,3 @@ echo "Home: /dev/sda1 (200G)"
 echo "Binary cache: /dev/sda2 (100G)"
 echo "Portage tree: /dev/sda3 (50G)"
 echo "ISO storage: /dev/sda4 (580G)"
-
-# 3.1 Chuyển vào thư mục mount
-cd /mnt/gentoo
-
-# 3.2 Tải stage3 mới nhất (chọn đúng kiến trúc)
-links https://www.gentoo.org/downloads/
-# Hoặc dùng wget:
-wget https://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-desktop-openrc.txt
-# Xem file vừa tải để biết tên stage3 chính xác
-
-# 3.3 Tải stage3
-wget [URL_STAGE3_TỪ_FILE_TRÊN]
-
-# 3.4 Giải nén
-tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
-
-
-
