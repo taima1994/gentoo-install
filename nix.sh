46bebb201873fef67512c937f263dad0ba0a6a54
taima <tantai01091994@gmail.com>
2025-12-29 11:31:58 +0000

    Create nix.sh

---
+++ nix.sh
@@ -0,0 +1,76 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "ghost";
+  home.homeDirectory = "/home/ghost";
+  home.stateVersion = "25.05";
+
+  # Các ứng dụng cơ bản
+  home.packages = with pkgs; [
+    # Terminal
+    kitty
+    alacritty
+    foot
+    
+    # Utilities
+    git
+    htop
+    neofetch
+    bat
+    eza
+    fzf
+    ripgrep
+    
+    # File manager
+    nautilus
+    ranger
+    
+    # Browser
+    firefox
+    google-chrome
+    
+    # Media
+    vlc
+    mpv
+    
+    # Documents
+    libreoffice
+    zathura
+    
+    # Graphics
+    gimp
+    inkscape
+    
+    # Communication
+    telegram-desktop
+    discord
+  ];
+
+  # Cấu hình Git
+  programs.git = {
+    enable = true;
+    userName = "Ghost";
+    userEmail = "ghost@localhost";
+  };
+
+  # Cấu hình Bash/Zsh
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -la";
+      update = "sudo nixos-rebuild switch";
+      clean = "sudo nix-collect-garbage -d";
+    };
+  };
+
+  # Cấu hình terminal (Kitty)
+  programs.kitty = {
+    enable = true;
+    theme = "Catppuccin-Mocha";
+    settings = {
+      font_family = "JetBrainsMono Nerd Font";
+      font_size = "12";
+      background_opacity = "0.9";
+    };
+  };
+}
